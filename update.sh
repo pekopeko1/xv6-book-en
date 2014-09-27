d692d15c364d05fb32c91913cc2043aac1d7afb0
pekopeko1 <t.moi.larsson+pekoepko1@gmail.com>
2014-09-28 05:07:17 +0900

    fix some utf-8 char

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 COMMIT_MSG=$1
 
+if [ x$COMMIT_MSG = x ];then
+	echo "Please input commit mesage."
+	echo "USAGE: $0 comit-message"
+	exit 1
+fi
 cp -Rp ../xv6-book/en/_build/html/* ./
 git add *
 git commit -m ${COMMIT_MSG}
